42d4e397474eb72cac1743c0a525a3c33ac4a23a
Loy_Dizak <loydizak@gmail.com>
2026-01-31 19:50:11 +0300

    Почистил json_handler

--- source/json_handler.py
+++ source/json_handler.py
@@ -1,37 +1,14 @@
 import json
 from dataclasses import asdict
-from parser import Person
-import logging
+from data import Person
 
-def save_persons_to_json(
-    file_path: str,
-    persons: list[Person],
-    ensure_ascii: bool = False,
-    indent: int = 4
-) -> None:
-    """
-    Сохраняет список объектов Person в JSON-файл.
-
-    ensure_ascii=False — корректная запись кириллицы
-    indent — удобочитаемое форматирование
-    """
-    
-    logging.info(f"Сохраняю новый файл по пути: \"{file_path}\"")
+def save_persons_to_json(file_path: str, persons: list[Person]) -> None:
     data = [asdict(person) for person in persons]
 
-    with open(file_path, "w", encoding="utf-8") as f:
-        json.dump(
-            data,
-            f,
-            ensure_ascii=ensure_ascii,
-            indent=indent
-        )
+    with open(file_path, "w", encoding="utf-8") as file:
+        json.dump(data, file, ensure_ascii=False, indent=2)
 
 def load_persons_from_json(file_path: str) -> list[Person]:
-    """
-    Считывает JSON-файл и возвращает список объектов Person.
-    """
-    logging.info(f"Открываю файл по пути: \"{file_path}\"")
     with open(file_path, "r", encoding="utf-8") as f:
         data = json.load(f)
 
