b102a32ad7e466f5b9d37f4c29b725ca3183b137
Loy_Dizak <loydizak@gmail.com>
2026-02-01 17:39:59 +0300

    Добавил билдфайл для генератора ключей

---
+++ build_key_generatir.bat
@@ -0,0 +1,29 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set "ENTRY_POINT=source\key_generator\front.py"
+set "APP_NAME=Key generator"
+set "TARGET_DIR=builds\key_generator\"
+set "BUILD_NAME=%APP_NAME%.exe"
+
+python3 -m PyInstaller --onefile --noconsole --name="%APP_NAME%" %ENTRY_POINT%
+if not exist "%TARGET_DIR%" mkdir "%TARGET_DIR%"
+
+REM Change file name if it already exists
+set "NEW_NAME=%BUILD_NAME%"
+set /A count=1
+:check_exists
+if exist "%TARGET_DIR%\!NEW_NAME!" (
+    set "BASENAME=%APP_NAME% (!count!)"
+    set "NEW_NAME=!BASENAME!.exe"
+    set /A count+=1
+    goto check_exists
+)
+
+move /Y "dist\%BUILD_NAME%" "%TARGET_DIR%\!NEW_NAME!"
+
+rmdir /S /Q build
+rmdir /S /Q dist
+del "%APP_NAME%.spec"
+
+pause
